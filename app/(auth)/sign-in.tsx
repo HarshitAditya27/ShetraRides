716df68a190fb0c714d5a76be879f0d9d9d7b17b
Harshit Aditya <harshitaditya27@gmail.com>
2024-10-29 16:21:22 +0530

    Added bottom tab component

--- app/(auth)/sign-in.tsx
+++ app/(auth)/sign-in.tsx
@@ -42,7 +42,7 @@ function SignIn() {
       <View className="flex-1 bg-white">
         <View className="relative w-full h-[250px]">
           <Image source={images.signUpCar} className="z-0 w-full h-[250px]" />
-          <Text className="text-2xl text-black font-JakartaSemiBold absolute bottom-5 left-5">
+          <Text className="text-2xl text-black font-bold absolute bottom-5 left-5">
             Namaste
           </Text>
         </View>
--- app/(auth)/sign-up.tsx
+++ app/(auth)/sign-up.tsx
@@ -77,7 +77,7 @@ function SignUp() {
       <View className="flex-1 bg-white">
         <View className="relative w-full h-[250px]">
           <Image source={images.signUpCar} className="z-0 w-full h-[250px]" />
-          <Text className="text-2xl text-black font-JakartaSemiBold absolute bottom-5 left-5">
+          <Text className="text-2xl text-black font-bold absolute bottom-5 left-5">
             Create Your Account
           </Text>
         </View>
